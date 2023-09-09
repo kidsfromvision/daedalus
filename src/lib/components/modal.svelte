eaaa4ecd5b9979f08fcae7ed5d2f8c8f78b0de13
meyad-g <79023203+meyad-g@users.noreply.github.com>
2023-09-09 18:08:11 +0100

    fix mobile responsiveness

--- src/lib/components/modal.svelte
+++ src/lib/components/modal.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { createEventDispatcher, onMount } from 'svelte';
+	import { fly, fade } from 'svelte/transition';
 
 	const dispatch = createEventDispatcher();
 
@@ -10,17 +11,33 @@
 	const zIndex = 50; // Fixed z-index
 
 	onMount(() => {
-		document.addEventListener('keydown', handleKeyDown);
-		return () => {
-			document.removeEventListener('keydown', handleKeyDown);
-		};
+		if (typeof document !== 'undefined') {
+			document.addEventListener('keydown', handleKeyDown);
+			// this is so you can't scroll the background when the modal is open
+			if (isOpen) {
+				document.body.style.overflow = 'hidden';
+			} else {
+				document.body.style.overflow = '';
+			}
+			return () => {
+				document.removeEventListener('keydown', handleKeyDown);
+				document.body.style.overflow = '';
+			};
+		}
 	});
+
+	$: if (typeof document !== 'undefined') {
+		if (isOpen) {
+			document.body.style.overflow = 'hidden';
+		} else {
+			document.body.style.overflow = '';
+		}
+	}
 
 	function handleKeyDown(event: KeyboardEvent): void {
 		if (event.key === 'Escape') {
 			close();
 		}
-		// Add further key-based handlers if needed.
 	}
 
 	function close(): void {
@@ -37,42 +54,66 @@
 			close();
 		}
 	}
+	let yValue: number;
+
+	if (typeof window !== 'undefined') {
+		yValue = window.innerHeight;
+	}
 </script>
 
-<div
-	class="{isOpen ? 'fixed' : 'hidden'} inset-0 flex items-center justify-center z-{zIndex}"
-	style="background-color: rgba(0,0,0,0.5);"
-	on:click={close}
-	on:keydown={handleBackgroundKeyDown}
-	role="button"
-	tabindex="0"
->
+{#if isOpen}
 	<div
-		class="bg-white rounded-lg shadow-md max-w-lg w-full p-6 relative overflow-auto max-h-screen"
-		on:click|stopPropagation
+		class="fixed inset-0 flex items-center justify-center z-{zIndex} bg-black bg-opacity-50"
+		on:click={close}
 		on:keydown={handleBackgroundKeyDown}
 		role="button"
 		tabindex="0"
+		in:fade={{ duration: 150 }}
+		out:fade={{ duration: 150 }}
 	>
-		{#if showCloseButton}
-			<button class="absolute top-2 right-2" on:click={close}>X</button>
-		{/if}
-		<slot />
-		{#if submitLabel}
-			<button on:click={submit} class="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
-				>{submitLabel}</button
-			>
-		{/if}
+		<div
+			class="bg-white rounded-lg shadow-md max-w-lg w-full p-6 relative max-h-screen"
+			on:click|stopPropagation
+			on:keydown={handleBackgroundKeyDown}
+			role="button"
+			tabindex="0"
+			in:fly={{ y: yValue, duration: 350 }}
+			out:fly={{ y: yValue, duration: 200 }}
+		>
+			{#if showCloseButton}
+				<button class="absolute top-2 right-2" on:click={close}>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke-width="1.5"
+						stroke="currentColor"
+						class="w-6 h-6"
+					>
+						<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+					</svg>
+				</button>
+			{/if}
+			<div class="overflow-auto h-full">
+				<slot />
+				{#if submitLabel}
+					<button on:click={submit} class="mt-4 px-4 py-2 bg-blue-500 text-white rounded">
+						{submitLabel}
+					</button>
+				{/if}
+			</div>
+		</div>
 	</div>
-</div>
+{/if}
 
 <style>
-	/* Add media queries for mobile responsiveness */
 	@media (max-width: 640px) {
-		/* Here we make the modal almost full screen on small devices */
 		.bg-white {
-			margin: 0.5rem;
-			height: calc(100% - 1rem);
+			margin-top: 4rem;
+			width: 100%;
+			height: calc(100% - 4rem);
+			border-radius: 0.5rem 0.5rem 0 0;
+			overflow-y: auto; /* This ensures your modal is scrollable if the content exceeds its height */
 		}
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,6 +6,8 @@
 <button on:click={() => (showModal = true)}>Open Modal</button>
 
 <Modal bind:isOpen={showModal}>
-	<h1 class="text-xl font-bold mb-4">Modal Title</h1>
-	<p>This is the modal content.</p>
+	<div class="w-full h-full mt-2">
+		<h1 class="text-xl font-bold mb-4">Modal Title</h1>
+		<p>This is the modal content.</p>
+	</div>
 </Modal>
